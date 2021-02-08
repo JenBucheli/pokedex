89b459d9e692bf309f7625212d9cbb253115ccdd
Jen <jenifer.bucheli@gmail.com>
2021-02-08 21:45:23 +0100

    layout of the pokedex made,adding search bar

--- pokedex.js
+++ pokedex.js
@@ -1,1 +1,15 @@
-funtion
+
+function findPokemon(id,num) {
+    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
+        .then(function (response) {
+            response.json()
+                .then(function (pokemon) {
+
+                })
+        })
+}
+function creatPokemon(){
+    let item= document.querySelectorAll()
+}
+
+
