a13575f605fe2b77a6f877b04734884329880c76
Jen <jenifer.bucheli@gmail.com>
2021-02-09 08:57:09 +0100

    empty JS

--- pokedex.js
+++ pokedex.js
@@ -1,15 +1,2 @@
 
-function findPokemon(id,num) {
-    fetch(`https://pokeapi.co/api/v2/pokemon/${id}`)
-        .then(function (response) {
-            response.json()
-                .then(function (pokemon) {
-
-                })
-        })
-}
-function creatPokemon(){
-    let item= document.querySelectorAll()
-}
-
 
